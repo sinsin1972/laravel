7c65c6418b919441fb6dd714809b3abb5c4073a9
tenpointq7 <tenpoint.q7@gmail.com>
2024-10-09 11:09:45 +0900

    blog第６章まで

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.appi')
+@section('content')
+<p><a href="{{ route('articles.create') }}">記事を書く</a></p>
+@foreach ($articles as $article)
+<article class="article-item">
+    <div class="article-title">
+        <a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a>
+    </div>
+    <div class="article-info">{{ $article->created_at }}</div>
+</article>
+@endforeach
+@endsection()
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController; // 追加
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//追加
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+Route::patch('/articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
+Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
